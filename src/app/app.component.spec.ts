b9da11c6ee348c15b1909eef2216d0d0b79cc69f
Blake Bryant <bryantsoftwaredevelopment@gmail.com>
2021-10-03 23:43:54 -0600

    converted test to spectator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,30 +15,13 @@ describe('AppComponent', () => {
     const { component } = spectator;
     expect(component).toBeTruthy();
   })
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [
-  //       AppComponent
-  //     ],
-  //   }).compileComponents();
-  // });
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it(`should have as title 'testing-demo'`, () => {
+    const { component } = spectator;
+    expect(component.title).toEqual('testing-demo');
+  });
 
-  // it(`should have as title 'testing-demo'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('testing-demo');
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('testing-demo app is running!');
-  // });
+  it('should render title', () => {
+    expect(spectator.query('.content span')?.textContent).toContain('testing-demo app is running!');
+  });
 });
